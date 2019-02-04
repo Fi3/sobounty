909b06a4ea439098af37249a4ff9dd0f67ddfc9d
fi3 <fmerli1@gmail.com>
2019-02-04 17:23:41 +0000

    add engine

---
+++ api/.eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    "parser": "babel-eslint",
+    "env": {
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "flowtype"
+    ],
+    "rules": {
+        "indent": 0,
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    },
+    "globals": {
+      "$ReadOnlyArray": true
+    }
+};
